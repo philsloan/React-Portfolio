4c49d51aa99225a17d20896cc1393015ba67a15e
philsloan <pjs.tgj@gmail.com>
2024-03-04 12:27:44 -0500

    Server,app,index.html

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 // define routing
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming you have an App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
